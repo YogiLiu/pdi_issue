3d033d766246a1bf11cc3a5e98169f88f968ea08
YogiLiu <YogiLiu@outlook.com>
2025-03-23 20:46:36 +0800

    test: test repo container

--- src/pdi_issue/main.py
+++ src/pdi_issue/main.py
@@ -1,4 +1,4 @@
-from .container import SrvContainer
+from .container import SrvContainer, RepoContainer
 
 if __name__ == '__main__':
     container = SrvContainer()
@@ -8,3 +8,11 @@ if __name__ == '__main__':
     user_repo.do_something()
 
     container.shutdown_resources()
+
+    # container = RepoContainer()
+    # container.init_resources()
+
+    # user_repo = container.user()
+    # user_repo.do_something()
+
+    # container.shutdown_resources()
